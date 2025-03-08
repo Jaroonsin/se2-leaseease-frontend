4f6565c543daba36982d3ff708458611ca7bdcc0
Peace Chandol <peace.hyperion@gmail.com>
2025-03-09 00:14:46 +0700

    dog

--- store/store.ts
+++ store/store.ts
@@ -2,27 +2,29 @@ import { configureStore, Middleware } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import propertyReducer from './propertySlice';
 import autocompleteReducer from './autocompleteSlice';
+import reservationsReducer from './historySlice';
 
 const loggerMiddleware: Middleware = (storeAPI) => (next) => (action) => {
-    console.log('Dispatching action:', action);
-    const result = next(action);
-    console.log('New State:', storeAPI.getState());
-    return result;
+	console.log('Dispatching action:', action);
+	const result = next(action);
+	console.log('New State:', storeAPI.getState());
+	return result;
 };
 
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        property: propertyReducer,
+	reducer: {
+		auth: authReducer,
+		property: propertyReducer,
 		autocompleteReducer: autocompleteReducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loggerMiddleware),
+		reservations: reservationsReducer,
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(loggerMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export type AsyncThunkConfig = {
-    state: RootState;
-    rejectValue: string;
+	state: RootState;
+	rejectValue: string;
 };
