9633b2ec7164ca209e0bc9c20101c86ddd0184ae
jaroonro <charoonroj.amo@gmail.com>
2025-04-17 16:36:50 +0700

    new data type and funtion for admin

--- src/api/data/review.ts
+++ src/api/data/review.ts
@@ -6,10 +6,18 @@ export type reviewData = {
     rating: number;
     message: string;
     imageURL: string;
-	id: number;
-	lesseeID: number;
+    id: number;
+    lesseeID: number;
 };
-
+export type reviewDataForAdmin = {
+    name: string;
+    reviewedAt: string;
+    message: string;
+    imageURL: string;
+    id: number;
+    lesseeID: number;
+    pname: string;
+};
 export const convertToReviewData = (response: any): reviewData[] => {
     return response.data.reviews.map((item: any) => ({
         name: item.lessee_name, // Assuming propertyName is the name
@@ -17,8 +25,8 @@ export const convertToReviewData = (response: any): reviewData[] => {
         message: item.review_message,
         imageURL: item.imageURL,
         rating: item.rating,
-		id: item.id,
-		lesseeID: item.lesseeID,
+        id: item.id,
+        lesseeID: item.lesseeID,
     }));
 };
 export const getReviewData = async (propID: number) => {
@@ -34,4 +42,34 @@ export const getReviewData = async (propID: number) => {
         console.error('Error fetching reservations:', error);
         return data; // Return default data in case of error
     }
+};
+export const getReviewDataForAdmin = async (name: string, sort: string, dir: string) => {
+    const data: reviewDataForAdmin[] = [];
+    try {
+        const response = await apiClient.get(
+            `propertyReview/get/admin?page=1&pageSize=20&name=${name}&sort=${sort}&dir=${dir}`
+        );
+        console.log(response.data);
+        return convertToReviewDataForAdmin(response.data); // Return the fetched data
+    } catch (error) {
+        console.error('Error fetching reservations:', error);
+        return data; // Return default data in case of error
+    }
+};
+export const convertToReviewDataForAdmin = (response: any): reviewDataForAdmin[] => {
+    return response.data.reviews.map((item: any) => ({
+        name: item.lessee_name, // Assuming propertyName is the name
+        reviewedAt: item.time_stamp,
+        message: item.review_message,
+        imageURL: item.imageURL,
+        id: item.review_id,
+        lesseeID: item.lesseeID,
+        pname: item.property_name,
+    }));
+};
+export const deleteReview = async (id: number) => {
+    try {
+        const response = await apiClient.get(`propertyReview/delete/${id}`);
+    } catch (error) {}
+    return;
 };
