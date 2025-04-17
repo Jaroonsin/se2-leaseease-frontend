2d82ad29f5b084923665e388663a892bf7e3aee8
jaroonro <charoonroj.amo@gmail.com>
2025-04-17 16:37:58 +0700

    merge dev

--- app/users/dashboard/page.tsx
+++ app/users/dashboard/page.tsx
@@ -9,19 +9,18 @@ import LoadPage from '@/src/components/ui/loadpage';
 
 export default function DashboardPage() {
     const { user } = useAuth();
-    const [role, setRole] = useState<string | null>(null);
+    const [role, setRole] = useState<string | null>('admin');
 
     useEffect(() => {
         if (user) {
             setRole(user.role); // Example: 'lessee', 'lessor', 'admin'
         }
     }, [user]);
-
     if (!role) return <LoadPage></LoadPage>;
 
     switch (role) {
         case 'lessee':
-            return <LesseeDashboard />;
+            return <AdminDashboard />;
         case 'lessor':
             return <LessorDashboard />;
         case 'admin':
