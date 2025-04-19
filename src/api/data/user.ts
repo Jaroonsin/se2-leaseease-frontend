173de2be8f7e800124e10122b6a2bf10778ecaa2
jaroonro <charoonroj.amo@gmail.com>
2025-04-19 16:58:56 +0700

    user data for admin use

---
+++ src/api/data/user.ts
@@ -0,0 +1,45 @@
+import { apiClient } from '../axios';
+
+export type userData = {
+    id: number;
+    role: string;
+    name: string;
+    status: string;
+    address: string;
+    image_url: string;
+};
+
+export const convertToUserData = (response: any): userData[] => {
+    return response.data.map((item: any) => ({
+        id: item.id,
+        role: item.role,
+        name: item.name,
+        status: item.status,
+        address: item.address,
+        imageURL: item.imageURL,
+    }));
+};
+export const getRequestData = async (propID: number) => {
+    const data: userData[] = [];
+
+    if (propID === -1) return data;
+
+    try {
+        const response = await apiClient.get(`/admin/get-users`);
+        console.log(response.data);
+        return convertToUserData(response.data); // Return the fetched data
+    } catch (error) {
+        console.error('Error fetching users data:', error);
+        return data; // Return default data in case of error
+    }
+};
+export const updateUserStatus = async (id: number, status: string) => {
+    try {
+        const response = await apiClient.post(`/admin/manage-user/${id}`, {
+            status: status,
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error updating user status:', error);
+    }
+};
